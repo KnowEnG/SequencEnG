1b8e14f95411839bd72a23c18ed4dbbb728a5d40
Yi <wingsyiz@gmail.com>
2018-07-06 12:27:01 -0500

    data update; hide scRNA-seq

--- js/tree.js
+++ js/tree.js
@@ -10,13 +10,14 @@ const TREE_HEIGHT = 1500;
 
 
 //intro
-const introTragetOne = "Epigenetics";
-const introTargetTwo = "TF Binding";
+const introTragetOne = "DNA";
+const introTargetTwo = "TF binding";
 const introTargetThree = "TF_ChIP-seq";
 
 const has_step = ["WGS", "WES", "RNA-seq", "CLIP-seq", "TF_ChIP-seq", "Histone_ChIP-seq", "DNase-seq", "ATAC-seq","ChIA-PET", "Hi-C", "WGBS"];
 
-const scRNA = ["Drop-seq", "10X", "Smart-seq", "Microwell-seq", "sci-RNA-seq"];
+const scRNA = [];
+// const scRNA = ["Drop-seq", "10X", "Smart-seq", "Microwell-seq", "sci-RNA-seq"];
 
 var nodeDepthOne = 150;
 var nodeDepthTwo = 220;
